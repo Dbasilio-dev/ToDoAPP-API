d11e7695783adcfac47c5784a9cfa5d10e6eb208
Dbasilio-dev <dbasiliodev@gmail.com>
2021-02-19 03:36:32 -0300

    ++Mongo implementado. --Removendo banco de dados anterior.

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
+// require("dotenv").config({ path: __dirname + "./.env" });
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = 8080;
 const chalk = require("chalk");
 
-const usuariosController = require("./controllers/usuarios-controller");
-const tarefasController = require("./controllers/tarefas-controller");
-const mainController = require("./controllers/main-controller");
+// const usuariosController = require("./controllers/usuarios-controller");
+// const tarefasController = require("./controllers/tarefas-controller");
+// const mainController = require("./controllers/main-controller");
 
-usuariosController(app);
-tarefasController(app);
-mainController(app);
+// usuariosController(app);
+// tarefasController(app);
+// mainController(app);
+
+mongoose.connect("mongodb://localhost/ToDo", { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () =>
+  console.log(chalk.green("[INFO]: Conectado ao Banco de Dados!"))
+);
 
 app.listen(port, () => {
-  console.log(chalk.green(`[INFO]: API rodando na porta:${port} com sucesso!`));
+  console.log(chalk.green(`[INFO]: Servidor rodando na porta:${port}!`));
 });
